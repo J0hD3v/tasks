be0c23ca6150948358e6cf58caf5402103510ba2
J0hD3v <johanfievet@orange.fr>
2025-09-10 09:40:29 +0200

    Tout dernier fix

--- cypress/e2e/oldTest.cy.js
+++ cypress/e2e/oldTest.cy.js
@@ -1,13 +1,13 @@
-describe('template spec', () => {
+describe.skip('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:8000')
+    cy.visit('http://127.0.0.1:8000')
   }),
 
   // TEST : CREATION USER 
   xit('can create new user', () => {
 
     // Arrange (config)
-    cy.visit('http://localhost:8000/user/register')
+    cy.visit('http://127.0.0.1:8000/user/register')
     const message = "Le compte : test5@test.com a été ajouté en BDD";
     // Act (scénario)
     cy.get('[name="firstname"]').type('Firstname-test-5')
@@ -22,7 +22,7 @@ describe('template spec', () => {
   // TEST : UTILISATEUR EXISTANT
   it('cannot create already existing user', () => {
     // Arrange (config)
-    cy.visit('http://localhost:8000/user/register')
+    cy.visit('http://127.0.0.1:8000/user/register')
     const message = "Le compte existe déja";
     // Act (scénario)
     cy.get('[name="firstname"]').type('Firstname-test-2')
@@ -37,7 +37,7 @@ describe('template spec', () => {
   // TEST : CONNEXION UTILISATEUR
   it('can login user', () => {
     // Arrange (config)
-    cy.visit('http://localhost:8000/user/login')
+    cy.visit('http://127.0.0.1:8000/user/login')
     // Act (scénario)
     cy.get('[name="email"]').type('test@test.com')
     cy.get('[name="password"]').type('1234')
@@ -49,7 +49,7 @@ describe('template spec', () => {
   // TEST : ECHEC CONNEXION UTILISATEUR - FORMULAIRE INCOMPLET
   it('cannot login if form empty or partially empty', () => {
     // Arrange (config)
-    cy.visit('http://localhost:8000/user/login')
+    cy.visit('http://127.0.0.1:8000/user/login')
     const message = "Veuillez remplir les champs";
     // Act (scénario)
     cy.get('[name="email"]').type('test@test.com')
@@ -61,7 +61,7 @@ describe('template spec', () => {
   // TEST : ECHEC CONNEXION UTILISATEUR - IDENTIFIANTS INCORRECTS
   it('cannot login if incorrects email or password', () => {
     // Arrange (config)
-    cy.visit('http://localhost:8000/user/login')
+    cy.visit('http://127.0.0.1:8000/user/login')
     const message = "Les informations de connexion ne sont pas correctes";
     // Act (scénario)
     cy.get('[name="email"]').type('test@test.com')
